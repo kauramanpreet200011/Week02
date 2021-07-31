5d2b5f4fe9ab7ef4ea0fe430e270ef9e29ec4c92
Amanpreet Kaur <kauramanpreet200011@gmail.com>
2021-07-31 15:13:34 +0530

    Week 2

---
+++ week02/src/com/revature/optionals/Example.java
@@ -0,0 +1,11 @@
+package com.revature.optionals;
+
+public class Example {
+    public static void main(String[] args) {
+        String[] str = new String[10];  
+        //Getting the substring
+        String str2 = str[9].substring(2, 5);
+        //Displaying substring
+        System.out.print(str2);
+    }
+}
